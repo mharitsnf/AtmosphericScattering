840393291a3b90f4bf40d22285a9a10a5e9200de
Lonerangerix <1137198072@qq.com>
2022-06-07 15:24:07 +0200

    earth with shadows

--- shaders/model.fs
+++ shaders/model.fs
@@ -2,10 +2,26 @@
 out vec4 FragColor;
 
 in vec2 TexCoords;
-
+in vec3 Normal;  
+in vec3 FragPos;  
+uniform vec3 lightPos; 
+uniform vec3 lightColor;
 uniform sampler2D texture_diffuse1;
 void main()
 {    
-    FragColor = texture(texture_diffuse1, TexCoords);
+    // ambient
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+  	
+    // diffuse 
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+            
+    //vec3 result = (ambient + diffuse) * texture(texture_diffuse1, TexCoords);
+    vec3 result = (ambient + diffuse) *vec3(texture(texture_diffuse1, TexCoords).x,texture(texture_diffuse1, TexCoords).y,texture(texture_diffuse1, TexCoords).z);
+    FragColor = vec4(result, 1.0);
+    //FragColor = texture(texture_diffuse1, TexCoords);
 
 }
